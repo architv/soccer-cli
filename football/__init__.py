0a47b20252396ef3cd4e05b6a57676ebd2bc5f9a
Angelo Gladding <angelo@lahacker.net>
2015-09-02 12:31:49 -0700

    cleaned up a bit

--- football/__init__.py
+++ football/__init__.py
@@ -4,49 +4,32 @@ football scores
 """
 
 import json
-from pkg_resources import resource_string
+import pkg_resources
 
 import requests
 import click
 
 
 LIVE_URL = "http://soccer-cli.appspot.com/"
+
 BASE_URL = "http://api.football-data.org/alpha/"
 HEADERS = {"X-Auth-Token": ""}
-
-leauges = {"BL": 394,
-           "FL": 396,
-           "EPL": 398,
-           "LLIGA": 399,
-           "SA": 401,
-           "PPL": 402,
-           "DED": 404}
-teams = json.loads(resource_string("football", "clubs.json").decode("utf-8"))
+_data = pkg_resources.resource_string("football", "org.football-data.api.json")
+leagues, clubs = json.loads(_data.decode("utf-8"))
 
 
-def get_live_scores():
-    """
-    gets the live scores
-
-    """
+def print_live_scores():
+    """"""
     req = requests.get(LIVE_URL)
-    if req.status_code == 200:
-        scores = req.json()
-        if len(scores["games"]) == 0:
-            click.secho("No live action currently", fg="red", bold=True)
-            return
-        print_live_scores(scores)
-    else:
+    if req.status_code != 200:
         click.secho("There was problem getting live scores",
                     fg="red", bold=True)
-
-
-def print_live_scores(live_scores):
-    """
-    prints the live scores in a pretty format
-
-    """
-    for game in live_scores["games"]:
+        return
+    scores = req.json()
+    if len(scores["games"]) == 0:
+        click.secho("No live action currently", fg="red", bold=True)
+        return
+    for game in scores["games"]:
         click.echo()
         click.secho("%s\t" % game["league"], fg="green", nl=False)
         if game["goalsHomeTeam"] > game["goalsAwayTeam"]:
@@ -69,33 +52,21 @@ def print_live_scores(live_scores):
         click.echo()
 
 
-def get_team_scores(team, time):
-    """
-    queries the API and gets the particular team scores
-
-    """
+def print_club_scores(club, time):
+    """"""
     try:
-        team_id = teams[team][1]
+        club_id = clubs[club][1]
     except KeyError:
-        click.secho("No data for the team. Please check the team code.",
+        click.secho("No data for the club. Please check the club code.",
                     fg="red", bold=True)
-    if team_id:
-        uri_template = "{base_url}teams/{team_id}/fixtures?timeFrame=p{time}"
-        req = requests.get(uri_template.format(
-            base_url=BASE_URL, team_id=team_id, time=time), headers=HEADERS)
-        if req.status_code == 200:
-            print_team_scores(req.json())
-        else:
-            click.secho("No data for the team. Please check the team code.",
-                        fg="red", bold=True)
-
-
-def print_team_scores(team_scores):
-    """
-    prints the teams scores in a pretty format
-
-    """
-    for score in team_scores["fixtures"]:
+    uri_template = "{base_url}teams/{club_id}/fixtures?timeFrame=p{time}"
+    req = requests.get(uri_template.format(
+        base_url=BASE_URL, club_id=club_id, time=time), headers=HEADERS)
+    if req.status_code != 200:
+        click.secho("No data for the club. Please check the club code.",
+                    fg="red", bold=True)
+        return
+    for score in req.json()["fixtures"]:
         if score["status"] == "FINISHED":
             click.echo()
             click.secho("%s\t" % score["date"].split("T")[0],
@@ -119,80 +90,53 @@ def print_team_scores(team_scores):
             click.echo()
 
 
-def get_standings(league):
-    """
-    queries the API and gets the standings for a particular league
-
-    """
+def print_standings(league):
+    """"""
     if not league:
         click.secho("Please specify a league. e.g. `--standings --league=EPL`",
                     fg="red", bold=True)
         return
 
-    league_id = leauges[league]
+    league_id = leagues[league]
     uri_template = "{base_url}soccerseasons/{id}/leagueTable"
     req = requests.get(uri_template.format(base_url=BASE_URL, id=league_id),
                        headers=HEADERS)
-    if req.status_code == 200:
-        print_standings(req.json())
-    else:
+    if req.status_code != 200:
         click.secho("No data for the league. Please check the league code.",
                     fg="red", bold=True)
-
-
-def print_standings(league_table):
-    """
-    prints the league standings in a pretty way
-
-    """
-    line_template = ("{position:6}  {team:30}    {played:10}"
+        return
+    line_template = ("{position:6}  {club:30}    {played:10}"
                      "    {goaldiff:10}    {points:10}")
-    click.secho(line_template.format(position="POS", team="CLUB",
+    click.secho(line_template.format(position="POS", club="CLUB",
                                      played="PLAYED", goaldiff="GOAL DIFF",
                                      points="POINTS"))
-    for team in league_table["standing"]:
-        if team["position"] <= 4:
+    for club in req.json()["standing"]:
+        if club["position"] <= 4:
             colors = {"bold": True, "fg": "green"}
-        elif 5 <= team["position"] <= 17:
+        elif 5 <= club["position"] <= 17:
             colors = {"fg": "blue"}
         else:
             colors = {"fg": "red"}
-        click.secho(line_template.format(position=str(team["position"]),
-                                         team=str(team["teamName"]),
-                                         played=str(team["playedGames"]),
-                                         goaldiff=str(team["goalDifference"]),
-                                         points=str(team["points"])),
+        click.secho(line_template.format(position=str(club["position"]),
+                                         club=str(club["teamName"]),
+                                         played=str(club["playedGames"]),
+                                         goaldiff=str(club["goalDifference"]),
+                                         points=str(club["points"])),
                     **colors)
 
 
-def get_scores(league, time):
-    """
-    fetch scores based upon league and time
-
-    """
+def print_scores(league, time):
+    """"""
     if league:
-        league_id = leauges[league]
-        uri_template = ("{base_url}soccerseasons/{id}/"
-                        "fixtures?timeFrame=p{time}")
-        results = requests.get(uri_template.format(base_url=BASE_URL,
-                                                   id=league_id,
-                                                   time=str(time)),
-                               headers=HEADERS).json()
-        print_league_scores(results)
-        return
-    uri_template = "{base_url}fixtures?timeFrame=p{time}"
-    results = requests.get(uri_template.format(base_url=BASE_URL,
-                                               time=str(time)),
-                           headers=HEADERS).json()
-    print_league_scores(results)
-
-
-def print_league_scores(total_data):
-    """
-    prints the data in a pretty format
-
-    """
-    for data in total_data["fixtures"]:
+        league_id = leagues[league]
+        uri = "{base_url}soccerseasons/{id}/fixtures?timeFrame=p{time}"
+        req = requests.get(uri.format(base_url=BASE_URL, id=league_id,
+                                      time=str(time)), headers=HEADERS)
+    else:
+        uri = "{base_url}fixtures?timeFrame=p{time}"
+        req = requests.get(uri.format(base_url=BASE_URL, time=str(time)),
+                           headers=HEADERS)
+    for data in req.json()["fixtures"]:
         if data["result"]["goalsHomeTeam"] > data["result"]["goalsAwayTeam"]:
             click.secho("%-20s %-5d" % (data["homeTeamName"],
                                         data["result"]["goalsHomeTeam"]),
@@ -216,35 +160,31 @@ def print_league_scores(total_data):
               help="Shows live scores from various leagues")
 @click.option("-s", "--standings", is_flag=True,
               help="Standings for a particular league")
-@click.option("-l", "--league", type=click.Choice(leauges.keys()),
-              help="Choose the league whose fixtures you want to see. "
-                   "Bundesliga(BL), Premier League(EPL), La Liga (LLIGA),"
-                   "Serie A(SL), Ligue 1(FL), Eredivisie(DED), "
-                   "Primeira Liga(PPL))")
-@click.option("-t", "--team", type=click.Choice(teams.keys()),
-              help="Choose the team whose fixtures you want to see. See "
-                   "the various team codes listed on README)")
+@click.option("-l", "--league", type=click.Choice(leagues.keys()),
+              help="Choose the league whose fixtures you want to see: "
+                   "Bundesliga (BL), Premier League (EPL), La Liga (LLIGA),"
+                   "Serie A (SL), Ligue 1(FL), Eredivisie (DED), "
+                   "Primeira Liga (PPL)")
+@click.option("-c", "--club", type=click.Choice(clubs.keys()),
+              help="Choose the club whose fixtures you want to see.")
 @click.option("--time", default=6,
               help="The number of days for which you want to see the scores")
 @click.option("--auth", default="", help="Auth token")
-def main(league, time, standings, team, live, auth):
+def main(league, time, standings, club, live, auth):
     """
     a CLI for live and past football scores from various football leagues
 
     """
     global HEADERS
     HEADERS["X-Auth-Token"] = auth
-
     if live:
-        get_live_scores()
-        return
-    if standings:
-        get_standings(league)
-        return
-    if team:
-        get_team_scores(team, time)
-        return
-    get_scores(league, time)
+        print_live_scores()
+    elif standings:
+        print_standings(league)
+    elif club:
+        print_club_scores(club, time)
+    else:
+        print_scores(league, time)
 
 
 if __name__ == "__main__":
--- setup.py
+++ setup.py
@@ -1,17 +1,16 @@
 from setuptools import setup
 
-setup(name="football-scores",
+setup(name="football",
       version="0.3",
       description="league standings and live scores for world football",
       author="Archit Verma",
       license="MIT",
       classifiers=["Development Status :: 3 - Alpha",  # 4: beta, 5: stable
                    "Intended Audience :: Developers",
-                   "Topic :: Software Development :: Build Tools",
                    "License :: OSI Approved :: MIT License"],
       keywords="soccer football espn scores live tool cli",
       author_email="architv07@gmail.com",
       url="https://github.com/architv/soccer-cli",
-      install_requires=["click==5.0", "requests==2.7.0"],
-      package_data={"football": ["clubs.json"]},
+      install_requires=["click", "requests"],
+      package_data={"football": ["org.football-data.api.json"]},
       entry_points={"console_scripts": ["football = football:main"]})
