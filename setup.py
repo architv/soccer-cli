166edb3b92a8d70292f935cc67aaad01009ea204
Saturn <Saturn@users.noreply.github.com>
2015-09-08 06:07:15 +0100

    Fix formatting problems
    Use spaces instead of tabs across py files.
    Replace instances of two spaces for indentation with four spaces.
    Trailing whitespace removed.
    Two line breaks between end of function and next function.
    Remove space between decorator and function.

--- setup.py
+++ setup.py
@@ -4,42 +4,42 @@ from setuptools import setup
 import sys
 
 setup(
-  name='soccer-cli',
-  version='0.0.3.3',
-  description='Soccer for Hackers.',
-  author='Archit Verma',
-  license='MIT',
-  classifiers=[
-    # How mature is this project? Common values are
-    #   3 - Alpha
-    #   4 - Beta
-    #   5 - Production/Stablegit
-    'Development Status :: 3 - Alpha',
+    name='soccer-cli',
+    version='0.0.3.3',
+    description='Soccer for Hackers.',
+    author='Archit Verma',
+    license='MIT',
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stablegit
+        'Development Status :: 3 - Alpha',
 
-    # Indicate who your project is intended for
-    'Intended Audience :: Developers',
-    'Topic :: Software Development :: Build Tools',
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
 
-    # Pick your license as you wish (should match "license" above)
-     'License :: OSI Approved :: MIT License',
+        # Pick your license as you wish (should match "license" above)
+         'License :: OSI Approved :: MIT License',
 
-    # Specify the Python versions you support here. In particular, ensure
-    # that you indicate whether you support Python 2, Python 3 or both.
-    'Programming Language :: Python :: 2',
-    'Programming Language :: Python :: 2.6',
-    'Programming Language :: Python :: 2.7',
-  ],
-  keywords = "soccer football espn scores live tool cli",
-  author_email='architv07@gmail.com',
-  url='https://github.com/architv/soccer-cli',
-  py_modules=['main', 'leagueids', 'authtoken', 'teamnames', 'leagueproperties'],
-  install_requires=[
-    "click>=5.0",
-    "requests==2.7.0"
-  ] + (["colorama==0.3.3"] if "win" in sys.platform else []),
-  entry_points = {
-    'console_scripts': [
-        'soccer = main:main'
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
     ],
-  }
+    keywords = "soccer football espn scores live tool cli",
+    author_email='architv07@gmail.com',
+    url='https://github.com/architv/soccer-cli',
+    py_modules=['main', 'leagueids', 'authtoken', 'teamnames', 'leagueproperties'],
+    install_requires=[
+        "click>=5.0",
+        "requests==2.7.0"
+    ] + (["colorama==0.3.3"] if "win" in sys.platform else []),
+    entry_points = {
+        'console_scripts': [
+            'soccer = main:main'
+      ],
+    }
 )
