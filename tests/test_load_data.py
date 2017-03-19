24478a92ef1218ade7867ba5a2f2d8172aea8c64
Andrew Grant <andrewgrant243@gmail.com>
2017-03-18 21:30:36 -0400

    Implement simple unit tests for loading data at startup

---
+++ tests/test_load_data.py
@@ -0,0 +1,45 @@
+import unittest
+import sys
+sys.path.append('soccer')
+import leagueproperties
+import leagueids
+import main
+
+class TestLoadData(unittest.TestCase):
+
+    TEAMS_INFO_FILENAME = "teams.json"
+
+    def set_up(self):
+        pass
+
+    def tear_down(self):
+        pass
+
+    def test_load_team_data(self):
+        raised = False
+        try:
+            main.load_json(TestLoadData.TEAMS_INFO_FILENAME)["teams"]
+        except IOError:
+            raised = True
+        self.assertFalse(raised)
+
+    def test_load_league_properties(self):
+        raised = False
+        try:
+            league_properties = leagueproperties.LEAGUE_PROPERTIES
+            league_properties.keys()
+        except AttributeError:
+            raised = True
+        self.assertFalse(raised)
+
+    def test_load_league_ids(self):
+        raised = False
+        try:
+            leage_ids = leagueids.LEAGUE_IDS
+            leage_ids.keys()
+        except AttributeError:
+            raised = True
+        self.assertFalse(raised)
+
+if __name__ == '__main__':
+    unittest.main()
