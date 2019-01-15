d4c820ec22ef432f973e7a58f2883f62ff176fb4
Daniel <daniel@football-data.org>
2019-01-15 10:49:38 +0100

    bumped version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ if os.environ.get('USER','') == 'vagrant':
 
 setup(
     name='soccer-cli',
-    version='0.1.0.0',
+    version='0.1.1.0',
     description='Soccer for Hackers.',
     author='Archit Verma',
     license='MIT',
